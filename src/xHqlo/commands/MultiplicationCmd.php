3286906f98a21b9b6969449ba17fa2df4650b30b
xHqlo <v6uo50dl@gmail.com>
2026-06-21 10:24:43 +0200

    Add code for multiplication
    Code of Multiplication

--- src/xHqlo/commands/MultiplicationCmd.php
+++ src/xHqlo/commands/MultiplicationCmd.php
@@ -1,1 +1,39 @@
+<?php
 
+
+namespace xHqlo\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\lang\Translatable;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\player\Player;
+use xHqlo\Base;
+
+class MultiplicationCmd extends Command {
+
+
+    public function __construct()
+    {
+        parent::__construct("multiplication", "Permet de multiplié 2 chiffre");
+        $this->setPermission(DefaultPermissions::ROOT_USER);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+
+        if($sender instanceof Player){
+
+
+            if(count($args) < 2){
+                return;
+            }
+
+
+            $multiplication = $args[0] * $args[1];
+
+            $sender->sendMessage(Base::getInstance()->getConfig()->get("message-multiplication") . $multiplication . "");
+
+
+        }
+    }
+}
