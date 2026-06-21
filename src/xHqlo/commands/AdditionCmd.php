9e768637e31df905b59b0077b6ce52b261d4f601
xHqlo <v6uo50dl@gmail.com>
2026-06-21 10:24:10 +0200

    Update AdditionCmd.php
    Add code of Addition Command

--- src/xHqlo/commands/AdditionCmd.php
+++ src/xHqlo/commands/AdditionCmd.php
@@ -1,2 +1,38 @@
 <?php
 
+
+namespace xHqlo\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\lang\Translatable;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\player\Player;
+use xHqlo\Base;
+
+class AdditionCmd extends Command {
+
+
+    public function __construct()
+    {
+        parent::__construct("addition", "Permet d'additionné 2 chiffre");
+        $this->setPermission(DefaultPermissions::ROOT_USER);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+
+        if($sender instanceof Player){
+
+
+            if(count($args) < 2){
+                return;
+            }
+
+
+            $calcul = $args[0] + $args[1];
+            $sender->sendMessage(Base::getInstance()->getConfig()->get("message-addition") . $calcul . "");
+
+
+        }
+    }
+}
