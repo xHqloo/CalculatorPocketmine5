736719681078b8248da34235be5e2e0d590c89b6
xHqlo <v6uo50dl@gmail.com>
2026-06-21 10:23:34 +0200

    Update DivisionCmd.php
    Add code of Division Command

--- src/xHqlo/commands/DivisionCmd.php
+++ src/xHqlo/commands/DivisionCmd.php
@@ -1,1 +1,41 @@
 <?php
+
+
+namespace xHqlo\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\lang\Translatable;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\player\Player;
+use xHqlo\Base;
+
+class DivisionCmd extends Command {
+
+
+    public function __construct()
+    {
+        parent::__construct("division", "Permet de divisé 2 chiffre");
+        $this->setPermission(DefaultPermissions::ROOT_USER);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
+
+        if($sender instanceof Player){
+
+
+            if(count($args) < 2){
+                return;
+            }
+
+
+
+            $division = $args[0] / $args[1];
+
+
+            $sender->sendMessage(Base::getInstance()->getConfig()->get("message-divison") . $division . "");
+
+        }
+
+    }
+}
